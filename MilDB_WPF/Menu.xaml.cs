25e6939c84dc7cf64c3dc3979c4f8dcf4fd09916
tikklebit <147945768+tikklebit@users.noreply.github.com>
2025-05-14 19:25:50 +0300

    перероблений App.xaml

--- MilDB_WPF/Menu.xaml.cs
+++ MilDB_WPF/Menu.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MilDB_WPF;
 
@@ -11,7 +12,35 @@ public partial class Menu : Window
 
     private void AdminButton_Click(object sender, EventArgs e)
     {
-        this.Hide();
+        AdminButton.Visibility = Visibility.Collapsed;
+        AdminButton.IsEnabled = false;
+        UserButton.Visibility = Visibility.Collapsed;
+        UserButton.IsEnabled = false;
+        
+        AdminPassword.Visibility = Visibility.Visible;
+        AdminPassword.IsEnabled = true;
+        ConfirmButton.Visibility = Visibility.Visible;
+        ConfirmButton.IsEnabled = true;
+    }
+
+    private void AcceptButton_Click(object sender, EventArgs e)
+    {
+        if (AdminPassword.Password == "admin")
+        {
+            this.Hide(); 
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+        else
+        {
+            MessageBox.Show("Неправильний пароль!");
+            AdminPassword.Password = String.Empty;
+        }
+    }
+    
+    private void UserButton_Click(object sender, EventArgs e)
+    {
+        this.Hide(); 
         MainWindow main = new MainWindow();
         main.Show();
     }
